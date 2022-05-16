5ccbe9f1d32789632b821b64cb76849b83022d20
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-16 18:51:01 +0300

    features/add fetch and input request for fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+import { fetchImgs, searchParams } from './js/fetchImgs';
+
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+}
+
+refs.searchForm.addEventListener('submit', onClick);
+
+function onClick(e) {
+    e.preventDefault();
+    
+    const currentRequest = e.currentTarget.elements.searchQuery.value.trim();
+    searchParams.set('q', currentRequest);
+
+    fetchImgs().then(data => {
+        return data;
+    })
+}
+
