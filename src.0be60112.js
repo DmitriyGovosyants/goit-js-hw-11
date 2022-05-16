1f3d729494a1fc29c9cc92f45aa1b1abf5a3be07
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-16 15:51:52 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/goit-js-hw-11@5ccbe9f1d32789632b821b64cb76849b83022d20 🚀

--- src.0be60112.js
+++ src.0be60112.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
+},{}],"c6hd":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.fetchImgs=r,exports.searchParams=void 0;const e="https://pixabay.com/api",t="27444041-6ebfb7763dac999969343312e",a=new URLSearchParams({key:t,q:"",image_type:"photo",orientation:"horizontal",safesearch:"true"});function r(){return fetch(`${e}/?${a}`).then(e=>e.json())}exports.searchParams=a;
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.ea1146a8.js.map
+"use strict";require("./sass/main.scss");var e=require("./js/fetchImgs");const r={searchForm:document.querySelector("#search-form")};function s(r){r.preventDefault();const s=r.currentTarget.elements.searchQuery.value.trim();e.searchParams.set("q",s),(0,e.fetchImgs)().then(e=>e)}r.searchForm.addEventListener("submit",s);
+},{"./sass/main.scss":"clu1","./js/fetchImgs":"c6hd"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11/src.0be60112.js.map
