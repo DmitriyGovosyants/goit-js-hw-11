7564093f9e3561b76bbf7899a53186a6f61183c8
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-16 20:46:42 +0300

    update/add notify

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchImgs, searchParams } from './js/fetchImgs';
 
 const refs = {
@@ -9,11 +10,14 @@ refs.searchForm.addEventListener('submit', onClick);
 
 function onClick(e) {
     e.preventDefault();
-    
+
     const currentRequest = e.currentTarget.elements.searchQuery.value.trim();
     searchParams.set('q', currentRequest);
 
     fetchImgs().then(data => {
+        if (!data.total) {
+            return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
         return data;
     })
 }
--- src/js/fetchImgs.js
+++ src/js/fetchImgs.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://pixabay.com/api";
+const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "27444041-6ebfb7763dac999969343312e";
 
 const searchParams = new URLSearchParams({
@@ -6,11 +6,11 @@ const searchParams = new URLSearchParams({
     q: "",
     image_type: "photo",
     orientation: "horizontal",
-    safesearch: "true"
+    safesearch: "true",
 });
 
 function fetchImgs() {
-    return fetch(`${BASE_URL}/?${searchParams}`)
+    return fetch(`${BASE_URL}?${searchParams}`)
         .then(response => response.json())
 }
 
