a3fbc8e45f7fccf0c14729adbfb6cf6df9e5728d
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-17 14:21:10 +0300

    features/add css to gallery

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { fetchImgs, searchParams } from './js/fetchImgs';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
 }
 
 refs.searchForm.addEventListener('submit', onClick);
@@ -14,11 +15,44 @@ function onClick(e) {
     const currentRequest = e.currentTarget.elements.searchQuery.value.trim();
     searchParams.set('q', currentRequest);
 
-    fetchImgs().then(data => {
-        if (!data.total) {
-            return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        }
-        return data;
-    })
+    fetchImgs()
+        .then(data => {
+            if (!data.total) {
+                return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            }
+            console.log(data.hits)
+            renderImgs(data);
+        })
 }
 
+function renderImgs({hits}) {
+    const imgsMarkup = hits
+        .map((el) => {
+            const { webformatURL, tags, likes, views, comments, downloads } = el;
+            return `
+                <div class="photo-card">
+                    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${downloads}
+                        </p>
+                    </div>
+                </div>
+            `
+        })
+        .join('');
+    refs.gallery.innerHTML = imgsMarkup;
+}
